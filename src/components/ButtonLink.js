cb219091044a160ab6e2beb728c8f65f9588b5b3
Radek <tobolka@gmail.com>
2020-10-26 22:56:46 +0100

    Cleaning

--- src/components/ButtonLink.js
+++ src/components/ButtonLink.js
@@ -8,7 +8,6 @@ export default function ButtonLink({ children, href, className = '' }) {
         ${className}
         text-blue-600 
         hover:text-blue-800
-        
         transition
         duration-200
     `}
