8aa3467b528b241d8aa31e28365fe0c23f778ebb
Radek <tobolka@gmail.com>
2020-10-29 16:10:52 +0100

    Updated meta tags

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -3,21 +3,34 @@ import Footer from './Footer';
 import Header from './Header';
 import { Helmet } from 'react-helmet';
 
-export default function Layout({ children, title = 'Pano' }) {
+export default function Layout({ children, title = 'Pano - Data Transformation Simplified' }) {
   return (
     <>
       <Helmet>
         <title>{title}</title>
-        <meta name="description" content="Data transformation simplified" />
+        <meta
+          name="description"
+          content="Bridge the gap between raw data and valuable insights. The easy-to-use platform allows you to quickly map and transform data to fit your requirements."
+        />
+
+        <meta property="og:url" content="https://www.pano.dev" />
+        <meta property="og:type" content="website" />
         <meta property="og:title" content="Data Transformation Simplified" />
         <meta
           property="og:description"
           content="Bridge the gap between raw data and valuable insights. The easy-to-use platform allows you to quickly map and transform data to fit your requirements."
         />
         <meta property="og:image" content="https://www.pano.dev/pano.png" />
-        <meta property="og:url" content="https://www.pano.dev" />
-        <meta property="og:site_name" content="Pano.dev" />
-        <meta name="twitter:image:alt" content="Data transformation simplified" />)
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="pano.dev" />
+        <meta property="twitter:url" content="https://www.pano.dev" />
+        <meta name="twitter:title" content="Data Transformation Simplified" />
+        <meta
+          name="twitter:description"
+          content="Bridge the gap between raw data and valuable insights. The easy-to-use platform allows you to quickly map and transform data to fit your requirements."
+        />
+        <meta name="twitter:image" content="https://www.pano.dev/pano.png" />
       </Helmet>
       <Header />
       <main>{children}</main>
