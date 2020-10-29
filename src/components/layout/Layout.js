91248c08b46d9e11acd414ff7e8f92bc37145442
Radek <tobolka@gmail.com>
2020-10-29 14:37:48 +0100

    Adding meta tags

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -9,6 +9,12 @@ export default function Layout({ children, title = 'Pano' }) {
       <Helmet>
         <title>{title}</title>
         <meta name="description" content="Data transformation simplified" />
+        <meta property="og:title" content="Pano.dev" />
+        <meta property="og:description" content="Data transformation simplified" />
+ 
+        <meta property="og:url" content="https://www.pano.dev" />
+        <meta property="og:site_name" content="Pano.dev" />
+        <meta name="twitter:image:alt" content="Data transformation simplified" />)
       </Helmet>
       <Header />
       <main>{children}</main>
