edd03aa85d557b86e7d0eee855d7c47d027270ae
Radek <tobolka@gmail.com>
2020-11-11 18:36:22 +0100

    Updated navigation

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,30 +4,19 @@ import { Link } from 'gatsby';
 
 export default function Header() {
   return (
-    <header className="top-0 sticky my-4 bg-white z-30">
-      <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-3 px-8 font-medium">
-        <div className="lg:w-1/3">
-          <Link className="text-2xl" activeClassName="text-black" to="/">
-            pano<span className="font-light">.dev</span>
-          </Link>
-        </div>
-
-        <nav className="lg:w-1/3 flex mt-4 sm:mt-0 space-x-10 justify-center text-center">
-          {/* 
-         <Link activeClassName="text-black" to="/product">
-            Product
-          </Link>
-          */}
-        </nav>
-
-        <div className="lg:w-1/3 block space-x-8 place-items-end text-right">
-          <Link href="https://docs.pano.dev/" target="_blank" rel="noopener noreferrer">
+    <header className="bg-white z-30 w-full mt-4 top-0 sticky backdrop-blur bg-opacity-75">
+      <nav className="container mx-auto flex justify-between py-4 px-8">
+        <Link className="text-2xl" activeClassName="text-black" to="/">
+          pano<span className="font-light">.dev</span>
+        </Link>
+        <div className="space-x-8">
+          <a href="https://docs.pano.dev/" target="_blank" rel="noopener noreferrer">
             Docs
-          </Link>
-          <Link href="https://platform.panoramichq.com">Log in</Link>
+          </a>
+          <a href="https://platform.panoramichq.com">Log in</a>
           <Button to="/sign-up">Sign up for free</Button>
         </div>
-      </div>
+      </nav>
     </header>
   );
 }
